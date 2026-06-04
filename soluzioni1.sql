d19c8acc4aea1df2dfa4a60fc73815e4617f1f89
BelliaNino <bellianino1996@gmail.com>
2026-06-04 17:28:18 +0200

    ex n.2

--- soluzioni1.sql
+++ soluzioni1.sql
@@ -5,3 +5,11 @@
 SELECT *
 FROM `students`
 WHERE YEAR(`students`.`date_of_birth`) = 1990;
+
+/*
+2. Selezionare tutti i corsi che valgono più di 10 crediti 
+*/
+
+SELECT *
+FROM `courses`
+WHERE `courses`.`cfu` > 10;
